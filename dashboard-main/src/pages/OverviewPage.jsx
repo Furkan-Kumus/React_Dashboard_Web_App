0ea1fbef1ce41dc76ef88a227d9da7ce88b898d0
Furkan Kümüş <105515121+Furkan-Kumus@users.noreply.github.com>
2025-02-19 15:10:39 +0300

    sales chart added

--- dashboard-main/src/pages/OverviewPage.jsx
+++ dashboard-main/src/pages/OverviewPage.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 
-import Header from "../components/common/Header";
-import { motion } from "framer-motion";
-import StatCard from "../components/common/StatCard";
 import { Zap, Users, ShoppingBag, BarChart2 } from "lucide-react";
+import { motion } from "framer-motion";
+
+import Header from "../components/common/Header";
+import StatCard from "../components/common/StatCard";
+import SalesOverviewChart from '../components/overview/SalesOverviewChart';
 
 const OverviewPage = () => {
   return (
@@ -17,16 +19,17 @@ const OverviewPage = () => {
           animate={{ opacity: 1, y: 0 }}
           transition={{ duration: 1 }}
         >
-          {/* // TODO: Anasayfa değerleri statik yaptık değiştirmek için aşağıdakilerle oyna */}
+          {/* // TODO: 1-Anasayfa değerleri statik yaptık değiştirmek için aşağıdakilerle oyna */}
           <StatCard name="Total Sales" icon={Zap} value='$12,345' color='#6366F1' />
           <StatCard name='New Users' icon={Users} value='1,234' color='#885CF6' />
           <StatCard name='Total Products' icon={ShoppingBag} value='567' color='#EC4899' />
           <StatCard name='Conversion Rate' icon={BarChart2} value='12.5%' color='#10B981' />
         </motion.div>
 
-        <div>
-          
+        <div className="grid grid-cols-1 lg:grid-cols-2 gap-8">
+          <SalesOverviewChart />
         </div>
+
       </main>
     </div>
   );
